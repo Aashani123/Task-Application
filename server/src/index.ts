79bba015f054c5281af879f7cb689e6a7c8c30dc
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2024-03-04 09:19:21 +0530

    vercel updating new

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(bodyparser.json());
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
